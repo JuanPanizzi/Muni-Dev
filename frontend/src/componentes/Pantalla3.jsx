d4137a289f41e60be16b5c68d04e29fbcefd2db9
Juan <juanpanizzi@hotmail.com>
2024-06-03 11:23:28 -0300

    Function handleIndices updated

--- frontend/src/componentes/Pantalla3.jsx
+++ frontend/src/componentes/Pantalla3.jsx
@@ -89,7 +89,7 @@ export const Pantalla3 = () => {
   }
 
   //Esta funcion setea indiceGlobal e indices de los boxes
-  const hadleIndice = (setIndiceBoxNumber) =>{ 
+  const hadleIndices = (setIndiceBoxNumber) =>{ 
 
     setIndiceGlobal((prevIndice)=>{ 
       const newIndice = prevIndice + 1;
