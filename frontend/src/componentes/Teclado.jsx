59dd58a3fcebeb4df990584d3763c70f166b4e05
Juan <juanpanizzi@hotmail.com>
2024-07-02 17:01:02 -0300

    Teclado update

--- frontend/src/componentes/Teclado.jsx
+++ frontend/src/componentes/Teclado.jsx
@@ -6,8 +6,8 @@ const keys = [
   ['A', 'B', 'C', 'D', 'E', 'F', 'G'],
   ['H', 'I', 'J', 'K', 'L', 'M', 'N'],
   ['O', 'P', 'Q', 'R', 'S', 'T', 'U'],
-  ['V', 'W', 'X', 'Y', 'Z', '←', ' '],
-  ['✓']
+  ['V', 'W', 'X', 'Y', 'Z', '←', '✓' ],
+  [' ']
 ];
 
  const Teclado = ({ onKeyPress }) => {
@@ -34,23 +34,25 @@ const keys = [
   // );
   return (
     <div className="mt-3">
-      {keys.map((row, rowIndex) => (
-        <div key={rowIndex} className="w-2/3 m-auto">
-          <div className='px-2 pt-1 m-auto grid grid-cols-7 gap-2'>
-            {row.map((key) => (
-              <button 
-                key={key}
-                type={key === '✓' ? 'submit' : 'button'}
-                className={`h-[60px] mb-2 rounded-xl text-2xl ${key === '✓' ? 'bg-green-400' : key === '←' ? 'bg-red-400' : 'bg-cv-celeste-claro'}`}
-                onClick={() => onKeyPress(key)}
-              >
-                {key}
-              </button>
-            ))}
-          </div>
+    {keys.map((row, rowIndex) => (
+      <div key={rowIndex} className="w-2/3 m-auto">
+        <div className={`px-2 pt-1 m-auto grid ${rowIndex === keys.length - 1 ? 'grid-cols-2' : 'grid-cols-7'} gap-2`}>
+          {row.map((key) => (
+            <button 
+              key={key}
+              type={key === '✓' ? 'submit' : 'button'}
+              className={`h-[60px] mb-2 rounded-xl text-2xl ${
+                key === '✓' ? 'bg-green-400' : key === '←' ? 'bg-red-400' : key === ' ' ? 'col-span-6 bg-cv-celeste-claro' : 'bg-cv-celeste-claro'
+              }`}
+              onClick={() => onKeyPress(key)}
+            >
+              {key}
+            </button>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    ))}
+  </div>
   );
   };
 
