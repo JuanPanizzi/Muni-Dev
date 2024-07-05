0916a1b8e15d20eb9f02a20836c8bc397a11ab92
Juan <juanpanizzi@hotmail.com>
2024-07-05 08:51:15 -0300

    urls and home deleted

--- frontend/src/Rutas/Rutas.jsx
+++ frontend/src/Rutas/Rutas.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Pantalla3 } from '../componentes/Pantalla3'
-import { Home } from '../componentes/Home'
 import { Box } from '../componentes/Box'
 import { HomeTeclado } from '../componentes/HomeTeclado'
 // import Prueba from '../componentes/Prueba'
@@ -12,11 +11,8 @@ export const Rutas = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path='/' element={<Prueba />} /> */}
         <Route path='/' element={<HomeTeclado />} />
         
-        {/* <Route path='/' element={<Home />} /> */}
-        {/* <Route path='/ej' element={<Ej />} /> */}
         <Route path='/pantalla' element={<Pantalla3 />} />
         {/* <Route path='/print' element={<PrintButton />} /> */}
         <Route path='/box/:BoxId' element={<Box />} />
